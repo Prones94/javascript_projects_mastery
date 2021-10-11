3bee1ee465c93402e59c4fbcdb41900b23336ee7
Ian <ronesp.85@gmail.com>
2021-10-11 09:26:47 -0700

    [edit] Header and Hero components. Working on Project component

--- portfolio_website-STARTER/src/components/Hero/Hero.js
+++ portfolio_website-STARTER/src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Portfolio
+      </SectionTitle>
+      <SectionText>
+        My name is Ian Rones. I bring military professionalism with great frontend skills such as Javascript and React.
+      </SectionText>
+      <Button onClick={() => window.location = "https://google.com"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- portfolio_website-STARTER/src/components/Projects/Projects.js
+++ portfolio_website-STARTER/src/components/Projects/Projects.js
@@ -4,10 +4,40 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+const projectsDemo = [
+  {
+    title: 'Project 1',
+    description: 'Description of this project'
+  },
+  {
+    title: 'Project 2',
+    description: 'Description of this project'
+  },
+  {
+    title: 'Project 3',
+    description: 'Description of this project'
+  },
+
+]
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(({id, image, title, description, source, visit}) => (
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+        </BlogCard>
+      )
+      )}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
